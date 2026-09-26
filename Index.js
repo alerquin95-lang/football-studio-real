66b2373470a890924abf75de52426f7df43cc7ff
alerquin95-lang <alerquin95@gmail.com>
2026-09-26 14:13:23 +0200

    Delete Index.js

--- Index.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-const app = express();
-app.get('/', (req,res)=>res.send('<h1>Jokeria Cloud ONLINE! Coletando...</h1><p>Se voce esta vendo isso, deu certo! Agora vou colocar o coletor completo.</p>'));
-app.get('/api/rounds', (req,res)=>res.json({success:true, count:0, data:[STRIPPED]
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, ()=>console.log('Rodando na porta '+PORT));
